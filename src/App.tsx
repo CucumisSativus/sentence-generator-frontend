3f282ac39c0ecb96b75f6108c19ae6460ba9558c
Michał Gutowski <michal.gutowski@westwing.pl>
2018-05-28 20:36:50 +0200

    Add possibility to print haiku

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import HaikuApi from './api/HaikuApi';
 import HaikuRow from './components/HaikuRow';
 import GenerateHaikuForm from './components/GenerateHaikuForm';
 import CurrentHaikuRow from './components/CurrentHaikuRow';
+import * as _ from 'lodash';
 import { Maybe } from 'tsmonad';
-import { Container, Row, Col, Navbar, NavbarBrand, Card, CardBody } from 'reactstrap';
+import { Container, Row, Col, Navbar, NavbarBrand, Card, CardBody, Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import PrintProvider, { Print, NoPrint } from 'react-easy-print';
 
 interface ApplicationState {
   newestHaiku: Maybe<Haiku>;
@@ -37,67 +39,102 @@ class App extends React.Component<{}, ApplicationState> {
     }));
   }
 
+  renderHaikuLines() {
+    let haikuLines = _.chunk(this.state.generatedHaikus, 3);
+    let toRender = haikuLines.map(haikuLine => {
+      let line = haikuLine.map(haiku => {
+        return (
+          <Col key={haiku.id} md={4}>
+            <Card>
+              <CardBody>
+                <HaikuRow
+                  id={haiku.id}
+                  firstLine={haiku.firstLine}
+                  middleLine={haiku.middleLine}
+                  lastLine={haiku.lastLine}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+
+        );
+      });
+      let lineId = haikuLine.map(l => { return l.id; }).toString();
+      return (
+        <div key={lineId}>
+          < div className="row align-items-center">
+            {line}
+          </div>
+          <div className="row-spacer" />
+        </div >
+      );
+    });
+
+    return toRender;
+  }
+
+  renderCurrentHaikuRow() {
+    return this.state.newestHaiku.map(haiku => {
+      return (
+        <CurrentHaikuRow
+          key={haiku.id}
+          id={haiku.id}
+          firstLine={haiku.firstLine}
+          middleLine={haiku.middleLine}
+          lastLine={haiku.lastLine}
+        />
+      );
+    }).caseOf({
+      just: s => s,
+      nothing: () => <p />
+    });
+  }
+
+  renderPrintButton() {
+    return this.state.newestHaiku.caseOf({
+      just: () => <Button color="info" onClick={() => window.print()}>Drukuj</Button>,
+      nothing: () => <span />
+    }); 
+  }
   render() {
     let onSubmit = (p: GenerateHaikuParams) => this.generateSentences(p);
     return (
-      <Container fluid={true}>
-        <div className="App container">
-          <Navbar>
-            <NavbarBrand>
-              Generator Haiku
+      <div>
+        <div className="example-print">
+          {this.renderCurrentHaikuRow()}
+        </div>
+        <div className="example-screen">
+          <Container fluid={true}>
+            <div className="App container">
+              <Navbar>
+                <NavbarBrand>
+                  Generator Haiku
           </NavbarBrand>
-          </Navbar>
-
-          <Row>
-            <Col>
-              {this.state.newestHaiku.map(haiku => {
-                return (
-                  <CurrentHaikuRow
-                    key={haiku.firstLine}
-                    firstLine={haiku.firstLine}
-                    middleLine={haiku.middleLine}
-                    lastLine={haiku.lastLine}
-                  />
-                );
-              }).caseOf({
-                just: s => s,
-                nothing: () => <p />
-              })}
+              </Navbar>
               <Row>
                 <Col>
-                  <GenerateHaikuForm
-                    onSubmit={onSubmit}
-                  />
+                  {this.renderCurrentHaikuRow()}
+                  <Row>
+                    <Col>
+                      <GenerateHaikuForm
+                        onSubmit={onSubmit}
+                      />
+                    </Col>
+                    <Col>
+                      {this.renderPrintButton()}
+                    </Col>
+                  </Row>
                 </Col>
               </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-            <Row>
-              {this.state.generatedHaikus.map(haiku => {
-                return (
-                  
-                    <Col key={haiku.firstLine} md={4}>
-                      <Card>
-                        <CardBody>
-                          <HaikuRow
-
-                            firstLine={haiku.firstLine}
-                            middleLine={haiku.middleLine}
-                            lastLine={haiku.lastLine}
-                          />
-                        </CardBody>
-                      </Card>
-                    </Col>
-                  
-                );
-              })}
+              <Row>
+                <Col>
+                  {this.renderHaikuLines()}
+                </Col>
               </Row>
-            </Col>
-          </Row>
+            </div>
+          </Container>
         </div>
-      </Container>
+      </div>
     );
   }
 }
--- src/components/CurrentHaikuRow.tsx
+++ src/components/CurrentHaikuRow.tsx
@@ -4,9 +4,9 @@ class CurrentHaikuRow extends React.Component<Haiku, {}> {
     render() {
         return (
             <div>
-                <h1 className="display-4">{this.props.firstLine}</h1>
-                <h1 className="display-4">{this.props.middleLine}</h1>
-                <h1 className="display-4">{this.props.lastLine}</h1>
+                <h1 className="display-3">{this.props.firstLine}</h1>
+                <h1 className="display-3">{this.props.middleLine}</h1>
+                <h1 className="display-3">{this.props.lastLine}</h1>
             </div>
         );
     }
